6a7eeb2a5154c423ede685cf6700aa52e9490d94
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-16 15:35:06 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-char str[] = ""Programming is like building a multilingual puzzle";
-	puts(str);
+puts("\"Programming is like building a multilingual puzzle");
+	
 	return 0;
 }
