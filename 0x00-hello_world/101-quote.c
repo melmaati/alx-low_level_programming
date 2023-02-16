5ee24a8c4a5dc6c7d7a6bfe5df8c7eab90885b70
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-16 17:10:53 +0200

    last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - a c program that prints a line to the standard error
- * Return: 1 (the program will be successful)
- */
+* main - a c program that prints a line to the standard error
+* Return: 1 (the program will be successful)
+*/
 int main(void)
 {
 	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
