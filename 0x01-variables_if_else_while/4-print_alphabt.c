522860ebc9b0541973a413ca85295e1d994d53c9
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-17 15:14:50 +0200

    alpha

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - print all the letters except q and e
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter != 'q' && letter != 'e')
+putchar(letter);
+}
+
+putchar('\n');
+
+return (0);
+}
