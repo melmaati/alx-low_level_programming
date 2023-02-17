47811578a6a9be1b56615cdc4cd84681c8f85c54
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-17 14:12:59 +0200

    task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,12 +19,12 @@ int main(void)
 	} 
 	else if (n > 0)
 	{
-		printf("%d is positive\n",n);
+		printf("%d is positive\n", n);
 	}
 	else (n < 0)
 	{
 		printf("%d is negative\n", n);
-	}
+	};
 
 	return (0);
 }
