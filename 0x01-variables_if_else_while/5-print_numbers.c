2092708b06b021ea460c6fdf46cee56953d51f40
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-17 15:17:55 +0200

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main -  main block
+* Description: print all single digit numbers of base 10
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int sum;
+
+for (sum = 0; sum < 10; sum++)
+printf("%d", sum);
+
+printf("\n");
+
+return (0);
+}
