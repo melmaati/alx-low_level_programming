5ed884b06b8f9793663bd865197aca13b797f5d0
Maati <mustaphaabouelmaaty@gmail.com>
2023-02-21 14:52:23 +0200

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* main - print _putchar
+* Description : Write a program that prints _putchar, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char printer[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(printer[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
